b882b23c984cd778bb5626d2721d8b4fce3b1177
Salman-Shaikh-92 <shaikhsalman742292@gmail.com>
2026-06-29 15:11:33 +0530

    reset api setting

--- firebase/firebaseClient.js
+++ firebase/firebaseClient.js
@@ -5,7 +5,7 @@ import { getAuth, GoogleAuthProvider, RecaptchaVerifier } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDlEeI1zLDZViKB1KWBPXQmGZKem7PT8_g" || '',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || '',
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || '',
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || '',
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || '',
